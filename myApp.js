b9287d0ee0a74174fd4d786378cd70f43c29c82e
Omar <omar.niazie@gmail.com>
2025-01-13 14:20:29 -0700

    57% done

--- myApp.js
+++ myApp.js
@@ -5,7 +5,12 @@ const app = express();
 const helmet = require('helmet');
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'DENY'}));
-
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
+app.use(helmet.dnsPrefetchControl());
 
 
 
